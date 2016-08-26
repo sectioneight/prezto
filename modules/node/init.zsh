804a7c694d0824435a1dc223f642814d462d3af2
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2016-08-26 12:28:52 +0100

    Lazy load nvm
    nvm takes on average half of a second to load,
    which is more than whole prezto takes to load.
    This can be noticed when you open a new shell.
    To avoid this, we are creating placeholder function for nvm, node,
    and all the node packages previously installed in the system to only
    load nvm when it is needed.
    
    This code is based on the scripts:
    * https://www.reddit.com/r/node/comments/4tg5jg/lazy_load_nvm_for_faster_shell_start/d5ib9fs
    * http://broken-by.me/lazy-load-nvm/
    * https://github.com/creationix/nvm/issues/781#issuecomment-236350067

--- modules/node/init.zsh
+++ modules/node/init.zsh
@@ -6,52 +6,59 @@
 #   Zeh Rizzatti <zehrizzatti@gmail.com>
 #
 
+lazy_load_nvm() {
+  NVM_DIR=$1
+  # Skip adding binaries if there is no node version installed yet
+  if [ -d $NVM_DIR/versions/node ]; then
+    NODE_GLOBALS=(`find $NVM_DIR/versions/node -maxdepth 3 \( -type l -o -type f \) -wholename '*/bin/*' | xargs -n1 basename | sort | uniq`)
+  fi
+  NODE_GLOBALS+=("nvm")
+
+  load_nvm () {
+    # Unset placeholder functions
+    for cmd in "${NODE_GLOBALS[@]}"; do unset -f ${cmd} &>/dev/null; done
+
+    # Load NVM
+    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+    # (Optional) Set the version of node to use from ~/.nvmrc if available
+    nvm use 2> /dev/null 1>&2 || true
+
+    # Do not reload nvm again
+    export NVM_LOADED=1
+  }
+
+  for cmd in "${NODE_GLOBALS[@]}"; do
+    # Skip defining the function if the binary is already in the PATH
+    if ! which ${cmd} &>/dev/null; then
+      eval "${cmd}() { unset -f ${cmd} &>/dev/null; [ -z \${NVM_LOADED+x} ] && load_nvm; ${cmd} \$@; }"
+    fi
+  done
+}
+
 # Load manually installed NVM into the shell session.
 if [[ -s "$HOME/.nvm/nvm.sh" ]]; then
-  source "$HOME/.nvm/nvm.sh"
+  lazy_load_nvm "$HOME/.nvm"
 
 # Load package manager installed NVM into the shell session.
-elif (( $+commands[brew] )) && \
-  [[ -d "${nvm_prefix::="$(brew --prefix 2> /dev/null)"/opt/nvm}" ]]; then
-  source "$(brew --prefix nvm)/nvm.sh"
-  unset nvm_prefix
-
-# Load manually installed nodenv into the shell session.
-elif [[ -s "$HOME/.nodenv/bin/nodenv" ]]; then
-  path=("$HOME/.nodenv/bin" $path)
-  eval "$(nodenv init - --no-rehash zsh)"
-
-# Load package manager installed nodenv into the shell session.
-elif (( $+commands[nodenv] )); then
-  eval "$(nodenv init - --no-rehash zsh)"
+elif (( $+commands[brew] )) && [[ -d "$(brew --prefix nvm 2>/dev/null)" ]]; then
+  lazy_load_nvm "$(brew --prefix nvm)"
 
 # Return if requirements are not found.
 elif (( ! $+commands[node] )); then
   return 1
 fi
 
-# Load NPM and known helper completions.
-typeset -A compl_commands=(
-  npm   'npm completion'
-  grunt 'grunt --completion=zsh'
-  gulp  'gulp --completion=zsh'
-)
+# Load NPM completion.
+if (( $+commands[npm] )); then
+  cache_file="${0:h}/cache.zsh"
 
-for compl_command in "${(k)compl_commands[@]}"; do
-  if (( $+commands[$compl_command] )); then
-    cache_file="${TMPDIR:-/tmp}/prezto-$compl_command-cache.$UID.zsh"
-
-    # Completion commands are slow; cache their output if old or missing.
-    if [[ "$commands[$compl_command]" -nt "$cache_file" \
-          || "${ZDOTDIR:-$HOME}/.zpreztorc" -nt "$cache_file" \
-          || ! -s "$cache_file" ]]; then
-      command ${=compl_commands[$compl_command]} >! "$cache_file" 2> /dev/null
-    fi
-
-    source "$cache_file"
-
-    unset cache_file
+  if [[ "$commands[npm]" -nt "$cache_file" || ! -s "$cache_file" ]]; then
+    # npm is slow; cache its output.
+    npm completion >! "$cache_file" 2> /dev/null
   fi
-done
 
-unset compl_command{s,}
+  source "$cache_file"
+
+  unset cache_file
+fi
